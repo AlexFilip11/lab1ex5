3e7ffca0356ea48b90e5e0c017c6282aab8ec2bb
Alex Filip <alexfilippaul@gmail.com>
2022-11-27 17:49:33 +0200

    citim 2 numere si le afisam crescator

---
+++ lab1ex5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lab1ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //citim doua numere si le afisar crescator
+            Console.WriteLine("Introduceti doua numere: ");
+            int x, y;
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+            if (x < y) Console.WriteLine(x + " " + y);
+            else Console.WriteLine(y + " " + x);
+
+        }
+    }
+}
